92a3d48c97cf4797e84da19bdce8eded4d1f79e0
JEstebanGuzman <etdb97@hotmail.com>
2025-07-16 17:25:40 -0500

    agregarImagenURLswagger

--- models/Producto.cs
+++ models/Producto.cs
@@ -3,8 +3,9 @@
     public class Producto
     {
         public int Id { get; set; }
-        public string Nombre { get; set; }
+        public string Nombre { get; set; } = string.Empty;
         public decimal Precio { get; set; }
+        public string ImagenUrl { get; set; } = string.Empty;
     }
 
 }
